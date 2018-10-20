1924406692643d98d874fccdd91d9b558b15ea56
Jill Morimoto <jillmorimoto@gmail.com>
2018-10-20 13:44:27 -0700

    Updates

---
+++ precache-manifest.59e9aa356ae9e5bb0b20c9422f99d637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "f6012acf91a07512470f",
+    "url": "/clickygame/static/js/main.f6012acf.chunk.js"
+  },
+  {
+    "revision": "bd67691d2662bbfc1b31",
+    "url": "/clickygame/static/js/1.bd67691d.chunk.js"
+  },
+  {
+    "revision": "f6012acf91a07512470f",
+    "url": "/clickygame/static/css/main.6f58bf5d.chunk.css"
+  },
+  {
+    "revision": "bd67691d2662bbfc1b31",
+    "url": "/clickygame/static/css/1.1d5777ac.chunk.css"
+  },
+  {
+    "revision": "e09422ec05ad719d8d0d49dd1281b11a",
+    "url": "/clickygame/index.html"
+  }
+];
